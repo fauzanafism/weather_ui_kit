4be24948ee0a4806fb10834c8536509ea7896a83
fauzanafism <fauzan137@gmail.com>
2022-02-21 06:46:22 +0700

    adjusted color of weather params widget

--- lib/presentation/widgets/weather_params.dart
+++ lib/presentation/widgets/weather_params.dart
@@ -15,15 +15,15 @@ class WeatherParams extends StatelessWidget {
           width: 85,
           child: Column(
             children: const [
-              Icon(Icons.air, color: Colors.white),
+              Icon(Icons.air, color: Colors.white70),
               SizedBox(
                 height: 8,
               ),
               Text('13 km/h',
-                  style: TextStyle(color: Colors.white)),
+                  style: TextStyle(color: Colors.white70)),
               Text(
                 'Wind',
-                style: TextStyle(color: Colors.white60),
+                style: TextStyle(color: Colors.white70),
               )
             ],
           ),
@@ -34,15 +34,15 @@ class WeatherParams extends StatelessWidget {
             children: const [
               FaIcon(
                 FontAwesomeIcons.thermometerThreeQuarters,
-                color: Colors.white,
+                color: Colors.white70,
               ),
               SizedBox(
                 height: 8,
               ),
-              Text('24 %', style: TextStyle(color: Colors.white)),
+              Text('24 %', style: TextStyle(color: Colors.white70)),
               Text(
                 'Humidity',
-                style: TextStyle(color: Colors.white60),
+                style: TextStyle(color: Colors.white70),
               )
             ],
           ),
@@ -52,14 +52,14 @@ class WeatherParams extends StatelessWidget {
           child: Column(
             children: const [
               FaIcon(FontAwesomeIcons.cloudRain,
-                  color: Colors.white),
+                  color: Colors.white70),
               SizedBox(
                 height: 8,
               ),
-              Text('87%', style: TextStyle(color: Colors.white)),
+              Text('87%', style: TextStyle(color: Colors.white70)),
               Text(
                 'Precipitation',
-                style: TextStyle(color: Colors.white60),
+                style: TextStyle(color: Colors.white70),
               )
             ],
           ),

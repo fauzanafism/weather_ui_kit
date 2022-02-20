4a9842c9f8a369a95e78102ed6f345405299d328
fauzanafism <fauzan137@gmail.com>
2022-02-21 06:38:12 +0700

    changed home to secondpage for testing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_ui_kit/presentation/pages/home_page.dart';
+import 'package:weather_ui_kit/presentation/pages/second_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage()
+      home: SecondPage()
     );
   }
 }

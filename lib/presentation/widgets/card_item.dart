9e9cea144f55d571c00dfcfa90d26eb256686ac3
fauzanafism <fauzan137@gmail.com>
2022-02-21 09:33:13 +0700

    reformatted code

--- lib/presentation/widgets/card_item.dart
+++ lib/presentation/widgets/card_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather_ui_kit/presentation/constant/ccolors.dart';
+import 'package:weather_ui_kit/presentation/constant/image_path.dart';
 
 class CardItem {
   final String temperature;
@@ -16,8 +17,8 @@ class CardItem {
 }
 
 final List<CardItem> cardItems = [
-  CardItem(temperature: '23\u00B0', imagePath: 'assets/images/day_partial_cloud.png', time: '10:00', color: Colors.black),
-  CardItem(temperature: '21\u00B0', imagePath: 'assets/images/rain_thunder.png', time: '11:00',color: Ccolors.firstBlue),
-  CardItem(temperature: '22\u00B0', imagePath: 'assets/images/day_rain.png', time: '12:00', color: Colors.black),
-  CardItem(temperature: '19\u00B0', imagePath: 'assets/images/day_rain.png', time: '13:00', color: Colors.black)
+  CardItem(temperature: '23\u00B0', imagePath: ImagePath.partialCloudDay, time: '10:00', color: Colors.black),
+  CardItem(temperature: '21\u00B0', imagePath: ImagePath.rainThunder, time: '11:00',color: Ccolors.firstBlue     ),
+  CardItem(temperature: '22\u00B0', imagePath: ImagePath.rainyDay, time: '12:00', color: Colors.black),
+  CardItem(temperature: '19\u00B0', imagePath: ImagePath.rainyDay, time: '13:00', color: Colors.black)
 ];

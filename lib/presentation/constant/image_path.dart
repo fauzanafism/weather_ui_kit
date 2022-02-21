45ba0fbb09ecc2bf19fc1f5d974ccde1f36e7eb2
fauzanafism <fauzan137@gmail.com>
2022-02-21 09:32:58 +0700

    added image path class

---
+++ lib/presentation/constant/image_path.dart
@@ -0,0 +1,9 @@
+class ImagePath {
+  static String rainyDay = 'assets/images/day_rain.png';
+  static String thunder = 'assets/images/thunder.png';
+  static String rainThunder = 'assets/images/rain_thunder.png';
+  static String cloudy = 'assets/images/cloudy.png';
+  static String clearDay = 'assets/images/day_clear.png';
+  static String partialCloudDay = 'assets/images/day_partial_cloud.png';
+  
+}
